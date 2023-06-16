5e4e6b59c37bcaa798bd091047a0c7a8e891aa3e
KingTugce <kingstugce@gmail.com>
2023-06-16 14:26:39 -0700

    change folder

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
+
+import Home from './pages/Home'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+
+function App() {
+
+
+  return (
+    
+      <div>
+        <Home/>
+      </div>
+    
+  )
+}
+
+export default App
