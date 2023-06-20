4b0d374d8e897fb9bf1d20f4b11de1ac936b251c
KingTugce <kingstugce@gmail.com>
2023-06-19 20:15:28 -0700

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,30 @@
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
+import routes from './config/route'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import About from './pages/About'
+
 
 function App() {
 
 
   return (
     
-      <div>
-        <Home/>
-      </div>
-    
+      <BrowserRouter>
+        {/*Navbar goes here*/}
+          <Routes>
+            { routes.map(( route, index) => (
+              <Route
+                key={index}
+                path={route.path}
+                element={
+                  <route.component/>
+                }
+              />
+            ) )}
+          </Routes>
+      </BrowserRouter>    
   )
 }
 
